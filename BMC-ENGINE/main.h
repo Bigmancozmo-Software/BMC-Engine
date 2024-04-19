73b7b14878ae64944505f9cc3ae1dadf80a318f5
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-04-19 18:59:36 -0400

    Add project files.

---
+++ BMC-ENGINE/main.h
@@ -0,0 +1,9 @@
+﻿// main.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+// TODO: Reference additional headers your program requires here.
