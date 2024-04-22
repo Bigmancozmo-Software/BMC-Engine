d642ab551e30ef439f988ae353a8301f55c69137
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-04-21 20:28:09 -0400

    make window

--- BMC-ENGINE/main.cpp
+++ BMC-ENGINE/main.cpp
@@ -12,5 +12,20 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
 	return 0;
 }
