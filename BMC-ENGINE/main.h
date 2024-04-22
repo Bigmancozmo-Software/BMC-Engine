d3e006593fcd345d9865cd3ab8a420fc13ac15a1
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-04-22 18:03:10 -0400

    ImGui includes

--- BMC-ENGINE/main.h
+++ BMC-ENGINE/main.h
@@ -1,7 +1,12 @@
 ﻿#pragma once
 
 #include <iostream>
+
+// Rendering
 #include "glad/glad.h"
 #include "glfw3.h"
+
+// ImGui
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
