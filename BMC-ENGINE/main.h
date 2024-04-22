15259bf3c88629be04b19371f34f478f3c2b1044
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-04-22 18:05:58 -0400

    move around

--- BMC-ENGINE/main.h
+++ BMC-ENGINE/main.h
@@ -1,12 +1,15 @@
 ﻿#pragma once
 
+// ImGui
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+// standard
 #include <iostream>
 
 // Rendering
 #include "glad/glad.h"
 #include "glfw3.h"
 
-// ImGui
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+
