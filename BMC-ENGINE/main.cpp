135c18c0694dbcdaf39ab22a64aba513c17380fd
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-04-23 20:00:19 -0400

    rainbow tri + shader fixes

--- BMC-ENGINE/main.cpp
+++ BMC-ENGINE/main.cpp
@@ -8,21 +8,21 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 const char* vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 position;\n"
-	"layout (location = 1) in vec3 color;\n"
-	"out vec4 vertexColor;\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 outColor;\n"
 	"void main()\n"
 	"{\n"
-	"	gl_Position = vec4(position, 1.0);\n"
-	"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+	"	gl_Position = vec4(aPos, 1.0);\n"
+	"	outColor = aColor;\n"
 	"}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 color;\n"
-	"in vec4 vertexColor;\n"
+	"in vec3 outColor;\n"
 	"void main()\n"
 	"{\n"
-	"	color = vertexColor;\n"
+	"	color = vec4(outColor, 1.0);\n"
 	"}\n\0";
 
 int main()
@@ -85,13 +85,9 @@ int main()
 
 
 	float vertices[] = {
-		-0.9f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f
-		-0.0f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,
-		-0.45f, 0.5f, 0.0f,    0.0f, 0.0f, 1.0f,
-
-		 0.0f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,
-		 0.9f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,
-		 0.45f, 0.5f, 0.0f,    0.0f, 1.0f, 1.0f
+		0.0f, 0.5f, 0.0f,				1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f,				0.0f, 1.0f, 0.0f,
+		0.5f, -0.5f, 0.0f,				0.0f, 0.0f, 1.0f
 	};
 
 	unsigned int VBO, VAO;
@@ -102,8 +98,13 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -122,8 +123,8 @@ int main()
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
-		glBindVertexArray(0);
-
+		glBindVertexArray(1);
+		/*
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
@@ -131,7 +132,7 @@ int main()
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
+		*/
 		glfwSwapBuffers(window);
 	}
 
