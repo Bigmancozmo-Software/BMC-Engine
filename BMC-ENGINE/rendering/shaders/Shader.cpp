f601a668dbda0299ddee409fb41eb8aef6f68d5b
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-04-23 20:23:34 -0400

    Add functions (blank)

---
+++ BMC-ENGINE/rendering/shaders/Shader.cpp
@@ -0,0 +1,21 @@
+#include "Shader.h"
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+}
+
+void Shader::use()
+{
+}
+
+void Shader::setBool(const std::string& name, bool value) const
+{
+}
+
+void Shader::setInt(const std::string& name, int value) const
+{
+}
+
+void Shader::setFloat(const std::string& name, float value) const
+{
+}
