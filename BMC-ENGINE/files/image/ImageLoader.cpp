057d4094d765765af1faddcb2abf920358bc3bc6
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-04-25 18:30:14 -0400

    remove image loader

--- BMC-ENGINE/files/image/ImageLoader.cpp
+++
@@ -1,7 +0,0 @@
-#include "ImageLoader.h"
-
-unsigned char* ImageLoader::loadImage(const char* path, int* img_width, int* img_height, int* img_channels)
-{
-	unsigned char* image = stbi_load("path_to_your_icon.png", img_width, img_height, img_channels, 4); // 4 = rgba
-	return image;
-}
