4486b25a42099d328d2243f4ad593ed96a9a707a
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-04-26 23:07:32 -0400

    params

--- BMC-ENGINE/main.cpp
+++ BMC-ENGINE/main.cpp
@@ -71,6 +71,11 @@ int main(int argc, char* argv[])
 		0.5f, -0.5f, 0.0f
 	};
 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
