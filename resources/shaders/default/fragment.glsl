89cf7bc8ab9d7c8fac63a4db18fe78a91fff1659
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-04-27 01:03:55 -0400

    yuh

--- resources/shaders/default/fragment.glsl
+++ resources/shaders/default/fragment.glsl
@@ -1,9 +1,13 @@
 #version 330 core
+out vec4 FragColor;
+  
+in vec3 ourColor;
+in vec2 TexCoord;
+in float multiplier;
 
-out vec4 color;
-in vec3 outColor;
+uniform sampler2D ourTexture;
 
 void main()
 {
-	color = vec4(outColor, 1.0);
+    FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0) * multiplier;
 }
