cd25e589394eb1b89cc0600f6b975f32644077d2
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-05-16 17:35:28 -0400

    reorder stuff

--- BMC-ENGINE/classes/Vector2.hpp
+++ BMC-ENGINE/classes/Vector2.hpp
@@ -4,10 +4,13 @@ class Vector2 {
 public:
 	int x, y;
 	
-	Vector2(int x, int y) {
-		this->x = x;
-		this->y = y;
-	}
+	Vector2(int x, int y);
 
 	Vector2 zero = new Vector2(0, 0);
 };
+
+Vector2::Vector2(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
