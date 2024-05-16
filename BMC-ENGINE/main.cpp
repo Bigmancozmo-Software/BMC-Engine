2ae46503e8a87ca8bae0d549cc57add061921e8a
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-05-16 16:09:23 -0400

    huh

--- BMC-ENGINE/main.cpp
+++ BMC-ENGINE/main.cpp
@@ -175,6 +175,19 @@ int main(int argc, char* argv[])
 	glfwSetWindowIcon(window, 1, app_icon);
 
 	glEnable(GL_DEPTH_TEST);
+
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 	
 	// main loop
 	while (!glfwWindowShouldClose(window))
@@ -195,7 +208,7 @@ int main(int argc, char* argv[])
 		glm::mat4 projection;
 		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-		int modelLoc = glGetUniformLocation(defaultShader .ID, "model");
+		int modelLoc = glGetUniformLocation(defaultShader.ID, "model");
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
 		int viewLoc = glGetUniformLocation(defaultShader.ID, "view");
@@ -204,12 +217,20 @@ int main(int argc, char* argv[])
 		int projectionLoc = glGetUniformLocation(defaultShader.ID, "projection");
 		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-
 		glBindTexture(GL_TEXTURE_2D, texture);
 		defaultShader.use();
 		glBindVertexArray(VAO);
-		//glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			cout << "spiny" << endl;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		glBindVertexArray(0);
 
 		if(useDebugger)
