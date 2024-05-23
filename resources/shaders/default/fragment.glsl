f25d332ae951843b14888f5743a4ed150f4d029d
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-05-23 17:29:59 -0400

    update shaders

--- resources/shaders/default/fragment.glsl
+++ resources/shaders/default/fragment.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 vertPos;
+
 void main()
 {
-    FragColor = vec4(0.0, 1.0, 1.0, 1.0);
+    FragColor = vec4(vertPos, 1.0);
 }
--- resources/shaders/default/vertex.glsl
+++ resources/shaders/default/vertex.glsl
@@ -1,7 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
+out vec3 vertPos;
+
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
+    vertPos = aPos; // send vertex position to fragment shader
 }
