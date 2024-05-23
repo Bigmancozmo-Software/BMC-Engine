cc52b8584ff08152e5d47d7918504d60f34dcae9
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-05-22 21:19:37 -0400

    some more code

--- BMC-ENGINE/main.cpp
+++ BMC-ENGINE/main.cpp
@@ -40,11 +40,18 @@ int main(int argc, char* argv[])
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// Connect EBO and indices array
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	// Tell OpenGL how to read coordinates
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// Bind VAO and VBO to 0
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	Styler::setToDefault();
 
