8af5aaebe44565ff0b8073d11579cd0dbf75e8d7
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-05-25 23:29:17 -0400

    crosscompiler fixes

--- BMC-ENGINE/classes/errors/ErrorHandler.cpp
+++ BMC-ENGINE/classes/errors/ErrorHandler.cpp
@@ -2,22 +2,18 @@
 
 void ErrorHandler::ErrorPopup(int type, const char* _text)
 {
-	bool windows = false;
-
-#if defined(WIN32)
-	windows = true;
-#endif
-
 	std::string textStr(_text);
 	std::string text = "   " + textStr + "\n   The program will be aborted.";
 
+#ifdef WIN32
 	switch (type) {
 	case ERROR_CRITICAL:
-		if (windows) { MessageBox(NULL, text.c_str(), "Critical Error", MB_ICONERROR | MB_OK); };
+		MessageBox(NULL, text.c_str(), "Critical Error", MB_ICONERROR | MB_OK);
 		glfwTerminate(); // terminate the program
 		break;
 	case ERROR_CAN_CONTINUE:
-		if (windows) { MessageBox(NULL, text.c_str(), "An Error Occurred", MB_ICONWARNING | MB_OK); };
+		MessageBox(NULL, text.c_str(), "An Error Occurred", MB_ICONWARNING | MB_OK);
 		break;
 	}
+#endif
 }
