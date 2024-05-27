c1b04a4582b920e58a09f99045eb76ede2927139
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-05-27 12:39:38 -0400

    Wierd stuff

--- BMC-ENGINE/rendering/graphics/Camera.cpp
+++ BMC-ENGINE/rendering/graphics/Camera.cpp
@@ -14,7 +14,7 @@ Camera::Camera(Vector2* size, glm::vec3 position)
 	this->width = size->x;
 }
 
-void Camera::matrix(float FOVdeg, float nearPlane, float farPlane, Shader* shader, const char* uniform)
+void Camera::updateMatrix(float FOVdeg, float nearPlane, float farPlane)
 {
 	glm::mat4 view = glm::mat4(1.0f);
 	glm::mat4 proj = glm::mat4(1.0f);
@@ -22,7 +22,12 @@ void Camera::matrix(float FOVdeg, float nearPlane, float farPlane, Shader* shade
 	view = glm::lookAt(position, position + orientation, up);
 	proj = glm::perspective(glm::radians(FOVdeg), (float)(width / height), nearPlane, farPlane);
 
-	shader->setMat4(uniform, proj * view);
+	cameraMatrix = proj * view;
+}
+
+void Camera::matrix(Shader* shader, const char* uniform)
+{
+	shader->setMat4(uniform, cameraMatrix);
 }
 
 void Camera::inputs(Window* window)
