305d045e302d651abb281c1f3d6cdb38279a907f
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-06-02 14:40:05 -0400

    change to use file reader

--- BMC-ENGINE/rendering/shaders/Shader.cpp
+++ BMC-ENGINE/rendering/shaders/Shader.cpp
@@ -3,33 +3,9 @@
 Shader::Shader(const char* vertexPath, const char* fragmentPath)
 {
     // 1. retrieve the vertex/fragment source code from filePath
-    std::string vertexCode;
-    std::string fragmentCode;
-    std::ifstream vShaderFile;
-    std::ifstream fShaderFile;
-    // ensure ifstream objects can throw exceptions:
-    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    try
-    {
-        // open files
-        vShaderFile.open(vertexPath);
-        fShaderFile.open(fragmentPath);
-        std::stringstream vShaderStream, fShaderStream;
-        // read file's buffer contents into streams
-        vShaderStream << vShaderFile.rdbuf();
-        fShaderStream << fShaderFile.rdbuf();
-        // close file handlers
-        vShaderFile.close();
-        fShaderFile.close();
-        // convert stream into string
-        vertexCode = vShaderStream.str();
-        fragmentCode = fShaderStream.str();
-    }
-    catch (std::ifstream::failure e)
-    {
-        std::cout << "Shader.cpp: Failed to read file!" << std::endl;
-    }
+    std::string vertexCode = FileReader::readFile(vertexPath);
+    std::string fragmentCode = FileReader::readFile(fragmentPath);
+
     const char* vShaderCode = vertexCode.c_str();
     const char* fShaderCode = fragmentCode.c_str();
 
