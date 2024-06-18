eea88f1501b21abc00e1dac2b6a701838a93883b
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-06-18 15:10:25 -0400

    Linux FMOD installer

---
+++ fmod-linux.sh
@@ -0,0 +1,21 @@
+cd ~
+ls
+
+sudo apt update
+sudo apt upgrade
+sudo apt --fix-broken install
+
+sudo apt install python3
+python3 --version
+
+sudo apt install python3-pip
+
+pip install gdown
+
+mkdir fmod
+cd fmod
+
+gdown 1zHVx19MbnNQnJKmf62zIQKONg0NFkWK2
+
+tar -xvzf fmodapi.tar.gz
+tar -xvzf fmodstudioapi20222linux.tar.gz
