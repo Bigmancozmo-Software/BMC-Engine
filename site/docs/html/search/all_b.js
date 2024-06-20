470dd22f04b268f3c5dd1b47bcd1106544e2fbc7
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-06-20 12:40:40 -0400

    Shader class docs

--- site/docs/html/search/all_b.js
+++ site/docs/html/search/all_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['unbind_0',['unbind',['../class_e_b_o.html#a8857542d7b10bc6ecd3aadd0567d4d42',1,'EBO::unbind()'],['../class_v_a_o.html#ae96e88a466b44d968315b5dc1f612946',1,'VAO::unbind()'],['../class_v_b_o.html#aad7aac5eb2e2d7d397b7f38bb33d3370',1,'VBO::unbind()'],['../class_texture.html#a8480eed7ed703a937c3e6ab528f559bd',1,'Texture::unbind()']]],
-  ['unit_1',['unit',['../class_texture.html#a9041cdeccaf234cd3945ef88e1092ebf',1,'Texture']]]
+  ['unit_1',['unit',['../class_texture.html#a9041cdeccaf234cd3945ef88e1092ebf',1,'Texture']]],
+  ['use_2',['use',['../class_shader.html#a870fa9f13d69e558815d6fd351a469dc',1,'Shader']]]
 ];
--- site/docs/html/search/functions_8.js
+++ site/docs/html/search/functions_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['unbind_0',['unbind',['../class_e_b_o.html#a8857542d7b10bc6ecd3aadd0567d4d42',1,'EBO::unbind()'],['../class_v_a_o.html#ae96e88a466b44d968315b5dc1f612946',1,'VAO::unbind()'],['../class_v_b_o.html#aad7aac5eb2e2d7d397b7f38bb33d3370',1,'VBO::unbind()'],['../class_texture.html#a8480eed7ed703a937c3e6ab528f559bd',1,'Texture::unbind()']]]
+  ['unbind_0',['unbind',['../class_e_b_o.html#a8857542d7b10bc6ecd3aadd0567d4d42',1,'EBO::unbind()'],['../class_v_a_o.html#ae96e88a466b44d968315b5dc1f612946',1,'VAO::unbind()'],['../class_v_b_o.html#aad7aac5eb2e2d7d397b7f38bb33d3370',1,'VBO::unbind()'],['../class_texture.html#a8480eed7ed703a937c3e6ab528f559bd',1,'Texture::unbind()']]],
+  ['use_1',['use',['../class_shader.html#a870fa9f13d69e558815d6fd351a469dc',1,'Shader']]]
 ];
