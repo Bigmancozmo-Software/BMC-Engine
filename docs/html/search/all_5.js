a9d990aecf80d7398f148ddebe092a27f0e21e0d
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-06-19 21:51:21 -0400

    docs generated

---
+++ docs/html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['initimgui_0',['initImGui',['../class_window.html#aacab3edab070f2ef946319d45d5b9db6',1,'Window']]]
+];
---
+++ docs/html/search/classes_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['filereader_0',['FileReader',['../class_file_reader.html',1,'']]]
+];
