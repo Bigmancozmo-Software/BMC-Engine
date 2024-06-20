6d0b8e4ca7480a3ef57f72409861244f1498736f
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-06-20 12:33:06 -0400

    Styling class docs

--- site/docs/html/search/all_9.js
+++ site/docs/html/search/all_9.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['seticon_0',['setIcon',['../class_window.html#acbb2e7275bd1b410c3e28c7ad010bf8b',1,'Window']]],
   ['setsize_1',['setSize',['../class_window.html#a21298af7780e4b1c43b5a8bc2e3cab18',1,'Window::setSize(int width, int height)'],['../class_window.html#a5202b1c60cd86d2a67436e38452a4e2e',1,'Window::setSize(Vector2 size)']]],
-  ['shader_2',['Shader',['../class_shader.html',1,'']]],
-  ['shouldclose_3',['shouldClose',['../class_window.html#ae779be3a5327f62c5e86a3e3d8a37289',1,'Window']]],
-  ['styler_4',['Styler',['../class_styler.html',1,'']]]
+  ['settodefault_2',['setToDefault',['../class_styler.html#ad7ba91d786c8b4c197e55d8b6eb442d0',1,'Styler']]],
+  ['shader_3',['Shader',['../class_shader.html',1,'']]],
+  ['shouldclose_4',['shouldClose',['../class_window.html#ae779be3a5327f62c5e86a3e3d8a37289',1,'Window']]],
+  ['styler_5',['Styler',['../class_styler.html',1,'']]]
 ];
