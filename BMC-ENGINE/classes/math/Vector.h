32a67cfb2f59f176a17ea20c02f0b85257883a7a
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-06-19 22:17:18 -0400

    Vector.h documentation

--- BMC-ENGINE/classes/math/Vector.h
+++ BMC-ENGINE/classes/math/Vector.h
@@ -3,22 +3,69 @@
 
 class Vector2 {
 public:
+	/// 
+	/// The X coordinate of the Vector2.
+	/// 
 	int x;
+
+	/// 
+	/// The Y coordinate of the Vector2.
+	/// 
 	int y;
 	
+	/// 
+	/// Create the Vector2
+	/// @param x The X coordinate to assign to the Vector2.
+	/// @param y The Y coordinate to assign to the Vector2.
+	/// 
 	Vector2(int x, int y);
+
+	/// 
+	/// Create the Vector2
+	/// @param xy The value to assign to the X and Y coordinates of the Vector2.
+	/// 
 	Vector2(int xy);
+
+	/// 
+	/// Create a blank Vector2, with X/Y = 0.
+	/// 
 	Vector2();
 };
 
 class Vector3 {
 public:
+	/// 
+	/// The X coordinate of the Vector3
+	/// 
 	int x;
+
+	/// 
+	/// The Y coordinate of the Vector3
+	/// 
 	int y;
+
+	/// 
+	/// The Z coordinate of the Vector3
+	/// 
 	int z;
 
+	/// 
+	/// Create the Vector2
+	/// @param x The X coordinate to assign to the Vector3.
+	/// @param y The Y coordinate to assign to the Vector3.
+	/// @param z The Z coordinate to assign to the Vector3.
+	/// 
 	Vector3(int x, int y, int z);
+
+	/// 
+	/// Create the Vector3
+	/// @param xyz The value to assign to the X/Y/Z coordinates of the Vector2.
+	///
 	Vector3(int xyz);
+
+	/// 
+	/// Create a blank Vector3, with X/Y/Z = 0.
+	/// 
 	Vector3();
 };
 
--- docs/html/search/all_9.js
+++ docs/html/search/all_9.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['vao_0',['VAO',['../class_v_a_o.html',1,'']]],
   ['vbo_1',['VBO',['../class_v_b_o.html',1,'']]],
-  ['vector2_2',['Vector2',['../class_vector2.html',1,'Vector2'],['../class_vector2.html#a321cf468f0b0caa050fc802f29af48ee',1,'Vector2::Vector2()']]],
-  ['vector3_3',['Vector3',['../class_vector3.html',1,'']]]
+  ['vector2_2',['Vector2',['../class_vector2.html',1,'Vector2'],['../class_vector2.html#a321cf468f0b0caa050fc802f29af48ee',1,'Vector2::Vector2(int x, int y)'],['../class_vector2.html#a393a25d8f437ff96f706be0a67b3ec10',1,'Vector2::Vector2(int xy)'],['../class_vector2.html#a22104d1809be26a419ef1f959e3761bf',1,'Vector2::Vector2()']]],
+  ['vector3_3',['Vector3',['../class_vector3.html',1,'Vector3'],['../class_vector3.html#af7339592cdacffdfc39566133bd285d0',1,'Vector3::Vector3(int x, int y, int z)'],['../class_vector3.html#a50d06a1259fbab25a9467d3273ad6055',1,'Vector3::Vector3(int xyz)'],['../class_vector3.html#a0f49191f7e001e7f7ae1cb49522118b4',1,'Vector3::Vector3()']]]
 ];
