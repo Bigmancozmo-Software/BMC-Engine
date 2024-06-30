f8e0bc0382179135e7d4f2109d1a640036182a37
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-06-30 11:09:14 -0400

    camera docs

--- BMC-ENGINE/rendering/graphics/Camera.h
+++ BMC-ENGINE/rendering/graphics/Camera.h
@@ -17,24 +17,85 @@
 
 class Camera {
 public:
+	/// 
+	/// The camera's position.
+	/// 
 	glm::vec3 position;
+
+	/// 
+	/// The camera's rotation.
+	/// 
 	glm::vec3 orientation = glm::vec3(0.0f, 0.0f, -1.0f);
+
+	/// 
+	/// The up vector.
+	/// 
 	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	/// 
+	/// The camera's matrix, which all vertices are multiplied by.
+	/// 
 	glm::mat4 cameraMatrix = glm::mat4(1.0f);
 
+	/// 
+	/// To be removed - ignore this.
+	/// 
 	bool firstClick = true;
 
-	int width;
-	int height;
+	/// 
+	/// The window's width.
+	/// 
+	int winWidth;
 
+	/// 
+	/// The window's height.
+	/// 
+	int winHeight;
+
+	/// 
+	/// The camera's movement speed.
+	/// 
 	float speed = 0.012f;
+
+	/// 
+	/// The camera's rotation sensitivity.
+	/// 
 	float sensitivity = 365.5f;
 
-	Camera(int width, int height, glm::vec3 position);
+	/// 
+	/// Creates the camera.
+	/// @param winWidth The window's width.
+	/// @param winHeight The window's height.
+	/// @param position The camera's position.
+	/// 
+	Camera(int winWidth, int winHeight, glm::vec3 position);
+
+	/// 
+	/// Creates the camera.
+	/// @param size The window's size, as a Vector3.
+	/// @param position The camera's position.
+	/// 
 	Camera(Vector2* size, glm::vec3 position);
 
+	/// 
+	/// Project the matrix.
+	/// @param FOVdeg The FOV, in degrees.
+	/// @param nearPlane How close to the camera objects can be before getting clipped.
+	/// @param farPlane How far away from the camera objects can be before getting clipped.
+	/// 
 	void updateMatrix(float FOVdeg, float nearPlane, float farPlane);
+
+	/// 
+	/// Send the camera matrix to the shader.
+	/// @param shader The vertex shader.
+	/// @param uniform The name of the matrix uniform.
+	/// 
 	void matrix(Shader* shader, const char* uniform);
+
+	/// 
+	/// Process the camera's inputs.
+	/// @param window The engine window.
+	/// 
 	void inputs(Window* window);
 };
 
