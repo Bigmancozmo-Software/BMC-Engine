7d471f6e2d1af595053a9308d015f66fac397cd2
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-07-12 02:20:29 -0400

    bugfix

--- BMC-ENGINE/classes/math/Vector.h
+++ BMC-ENGINE/classes/math/Vector.h
@@ -29,7 +29,7 @@ public:
 	Vector3 operator+(const Vector3& other) const { return Vector3(x + other.x, y + other.y, z + other.z); };
 	Vector3 operator-(const Vector3& other) const { return Vector3(x - other.x, y - other.y, z - other.z); };
 	Vector3 operator*(const Vector3& other) const { return Vector3(x * other.x, y * other.y, z * other.z); };
-	Vector3 operator/(const Vector3& other) const { return Vector3(x / other.x, y * other.y, z / other.z); };
+	Vector3 operator/(const Vector3& other) const { return Vector3(x / other.x, y / other.y, z / other.z); };
 };
 
 #endif // VECTOR_H
