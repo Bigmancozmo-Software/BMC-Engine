43274ec1db02e1974c1b18863937c4c321827234
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-07-12 01:27:02 -0400

    Adding

--- BMC-ENGINE/classes/math/Vector.h
+++ BMC-ENGINE/classes/math/Vector.h
@@ -9,6 +9,10 @@ public:
 	Vector2(int x, int y);
 	Vector2(int xy);
 	Vector2();
+
+	Vector2 operator+(const Vector2& other) const {
+		return Vector2(x + other.x, y + other.y);
+	};
 };
 
 class Vector3 {
@@ -20,6 +24,10 @@ public:
 	Vector3(int x, int y, int z);
 	Vector3(int xyz);
 	Vector3();
+
+	Vector3 operator+(const Vector3& other) const {
+		return Vector3(x + other.x, y + other.y, z + other.z);
+	};
 };
 
 #endif // VECTOR_H
