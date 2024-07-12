464869cffd277a2ca88a639eb491fd378c2113c4
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-07-12 01:35:42 -0400

    Add sub/mul/div

--- tests/vectors.cpp
+++ tests/vectors.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include "../BMC-ENGINE/classes/math/Vector.h"
 
+// Vector2 //
+
 TEST(Vector2Tests, Add) {
 	Vector2 added = Vector2(8, 5) + Vector2(3, 4);
 	Vector2 expected = Vector2(11, 9);
@@ -8,6 +10,32 @@ TEST(Vector2Tests, Add) {
 	EXPECT_EQ(added.x, expected.x);
 	EXPECT_EQ(added.y, expected.y);
 }
+
+TEST(Vector2Tests, Subtract) {
+	Vector2 added = Vector2(8, 5) - Vector2(3, 4);
+	Vector2 expected = Vector2(5, 1);
+
+	EXPECT_EQ(added.x, expected.x);
+	EXPECT_EQ(added.y, expected.y);
+}
+
+TEST(Vector2Tests, Multiply) {
+	Vector2 added = Vector2(8, 5) * Vector2(3, 4);
+	Vector2 expected = Vector2(24, 20);
+
+	EXPECT_EQ(added.x, expected.x);
+	EXPECT_EQ(added.y, expected.y);
+}
+
+TEST(Vector2Tests, Divide) {
+	Vector2 added = Vector2(8, 5) / Vector2(3, 4);
+	Vector2 expected = Vector2(8 / 3, 5 / 4);
+
+	EXPECT_EQ(added.x, expected.x);
+	EXPECT_EQ(added.y, expected.y);
+}
+
+// Vector3 //
 
 TEST(Vector3Tests, Add) {
 	Vector3 added = Vector3(5, 6, 3) + Vector3(8, 3, 7);
